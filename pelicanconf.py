9c55ac0c491213692dcf77b326a9d19ab85af9e5
kamuridesu <myk.gata14@gmail.com>
2022-08-17 19:08:29 -0300

    Add markdown to requirements

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,6 +33,18 @@ THEME = os.path.join("themes", "monospace")
 
 STATIC_PATHS = ['images', 'favicon.ico', 'documents']
 
+MARKUP = ('md',)
+# Markdown Configuration:
+MARKDOWN = {
+    'extension_configs': {
+	'markdown.extensions.codehilite': {'css_class': 'highlight'},
+	'markdown.extensions.toc' : {},
+	'markdown.extensions.extra': {},
+	'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
